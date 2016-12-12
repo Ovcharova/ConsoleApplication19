3d86db4d1c3462ae792c68052eabd1541c86dace
Vasiliy Cherniatyn <v.v.4ern9tin@gmail.com>
2016-12-12 20:25:40 +0200

    commit

---
+++ ConsoleApplication18/Article.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication18
+{
+    public class Article
+    {
+        public int Index;
+        public string Name;
+        public string Text;
+        public string Comment;
+        public DateTime TimeComment;
+
+        public override string ToString()
+        {
+            String format = "[MM/dd hh:mm]";
+            StringBuilder stringBilder = new StringBuilder();
+            stringBilder.AppendFormat("{0} ", Index)
+                .AppendFormat("{0} ", Name)
+                .AppendFormat("{0} ", Text)
+                .AppendFormat("{0} ", Comment)
+                .AppendFormat("{0} ", TimeComment.ToString(format));
+            return stringBilder.ToString();
+        }
+
+
+    }
+}
---
+++ ConsoleApplication18/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication18
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var articlesCollection = new List<Article>()
+                {
+                new Article()
+                {
+                    Index = 1,
+                    Name = "How to cook",
+                    Text = "For cooking ;skjgkjgkfjkssjksgjfhk",
+                    Comment = "",
+                    TimeComment = new DateTime()
+                },
+                new Article()
+                {
+                    Index = 2,
+                    Name = "How to eat",
+                    Text = "For eating ;skjgkjgkfjkssjksgjfhk",
+                    Comment = "",
+                    TimeComment = new DateTime()
+                },
+            };
+
+            Console.WriteLine("Number of Article For read: ");
+            string n;
+            int numberArticle = Console.ReadLine.Parse(n);
+
+            int numberArticle = -1;
+
+            int i = -1;
+            foreach (var item in articlesCollection)
+            {
+                i++;
+                if (numberArticle == Article.Index)
+                {
+                    numberArticle = i;
+                    break;
+                }
+                if (numberArticle == -1)
+                {
+                    Console.WriteLine("No such Article");
+                    Console.Beep();
+                    break;
+                }
+                Console.WriteLine(StringBilder.ToString());
+            }
+        }
+    }
